0abfdb17046977f6c44b6f9177a32f437c28674c
dengdeng <1304776291@qq.com>
2020-04-22 00:38:45 +0800

    路由基础搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,78 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/Home/index.vue'
+import Classify from '../views/Classify/index.vue'
+import Hello from '../views/Hello/index.vue'
+import Favorite from '../views/Hello/Favorite/index.vue'
+import history from '../views/Hello/History/index.vue'
+import Login from '../views/Login/index.vue'
+import My from '../views/My/index.vue'
+import Ranking from '../views/Ranking/index.vue'
+import Search from '../views/Search/index.vue'
+import Register from '../views/Register/index.vue'
+import SearchResult from '../views/SearchResult/index.vue'
+import Vip from '../views/Vip/index.vue'
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/classify',
+      component: Classify
+    },
+    {
+      path: '/hello',
+      component: Hello,
+      children: [
+        {
+          path: 'favorite',
+          component: Favorite
+        },
+        {
+          path: 'history',
+          component: history
+        },
+        {
+          path: '',
+          redirect: '/hello/favorite'
+        }
+      ]
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/my',
+      component: My
+    },
+    {
+      path: '/ranking',
+      component: Ranking
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/register',
+      component: Register
+    },
+    {
+      path: '/search-result',
+      component: SearchResult
+    },
+    {
+      path: '/vip',
+      component: Vip
+    },
+    {
+      path: '/',
+      redirect: '/home'
+    }
+  ]
 })
-
 export default router
---
+++ src/views/Vip/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="page-vip">
+      <h1>vip专区页面</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Vip'
+}
+</script>
